e4390ab4e17906713df348d521dcb48f0ae3362d
Flavio Augusto <augustof.cunha@outlook.com>
2020-08-28 19:34:33 -0300

    Add filter and backend connection

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,47 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Text } from 'react-native';
+import { TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+
+import api from '../../services/api';
 
 import styles from './styles';
-import { TextInput } from 'react-native-gesture-handler';
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
+  const [isfiltersVisible, setIsFiltersVisible] = useState(false);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekDay] = useState('');
+  const [time, setTime] = useState('');
+
+  function toggleFiltersVisible() {
+    setIsFiltersVisible(!isfiltersVisible);
+  }
+
+  function handleFiltersSubmit() {
+    api.get('classes', {
+      params: {
+          subject,
+          week_day,
+          time
+      }
+    }).then((res) => {
+      setTeachers(res.data);
+      setIsFiltersVisible(false);
+    }).catch((res) => {
+      if (res.response.status === 400)
+        alert('Faltam filtros. Por favor preencha todos.');
+      setTeachers([]);
+    })
+  }
+
   return (
-    <ScrollView>
+    // <ScrollView>
     <View style={styles.container} >
-      <PageHeader title="Proffys disponíveis" >
+      <PageHeader 
+        title="Proffys disponíveis" 
+        headerRight={(
+          <BorderlessButton onPress={toggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        )}>
 
-        <View style={styles.searchForm}>
-          <Text style={styles.label}>Matéria</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Qual a matéria?" 
-            placeholderTextColor='#c1b1cc'
-          />
+        { isfiltersVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              style={styles.input}
+              value={subject}
+              onChangeText={text => setSubject(text)}
+              placeholder="Qual a matéria?" 
+              placeholderTextColor='#c1b1cc'
+            />
 
-          <View style={styles.inputGroup}>
-            <View style={styles.inputBlock}>
-              <Text style={styles.label}>Dia da semana</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Qual o dia?" 
-                placeholderTextColor='#c1b1cc'
-              />
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput
+                  style={styles.input}
+                  value={week_day}
+                  onChangeText={text => setWeekDay(text)}
+                  placeholder="Qual o dia?" 
+                  placeholderTextColor='#c1b1cc'
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  style={styles.input}
+                  value={time}
+                  onChangeText={text => setTime(text)}
+                  placeholder="Qual horário?" 
+                  placeholderTextColor='#c1b1cc'
+                />
+              </View>
             </View>
-            
 
-            <View style={styles.inputBlock}>
-              <Text style={styles.label}>Horário</Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Qual horário?" 
-                placeholderTextColor='#c1b1cc'
-              />
-            </View>
+            <RectButton style={styles.submitButton} onPress={handleFiltersSubmit}>
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+
           </View>
-
-        </View>
+        )}
       </PageHeader>
 
       <ScrollView 
@@ -51,15 +100,11 @@ function TeacherList() {
           paddingBottom: 16
         }}
       >
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher:Teacher) => <TeacherItem key={teacher.id} teacher={teacher}/>)}
       </ScrollView>
 
     </View>
-    </ScrollView>
+    // </ScrollView>
   )
 }
 
